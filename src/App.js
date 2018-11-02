09b733e0ee82d4f1d63c2cc3b17293e6038fe51a
Rotimi Best <rotimiibitoyebest@yahoo.co.uk>
2018-11-02 14:41:54 +0200

    Changed logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './mecury.png';
+import logo from './neptune.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,17 +8,31 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to Meqry.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Log in
-          </a>
+          <div>
+            <h3>Welcome to Meqry.</h3>
+            <p>Bringing the future into the present </p>
+          </div>
+          <div >
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              style= {{margin: '10px'}}
+            >
+              Log in
+            </a>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              style= {{margin: '10px'}}
+            >
+              Sign up
+            </a>
+          </div>
+
         </header>
       </div>
     );
